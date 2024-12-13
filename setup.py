e31949e427b723a1d98b949af7ceaf418c6722c0
Gari Ciodaro <gari.ciodaro@recall.space>
2024-12-13 07:43:24 +0100

    relax constraints

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="agent-builder",
-    version="0.0.1",
+    version="0.0.2",
     author="Recall Space",
     author_email="info@recall.space",  
     description="The agent-builder package to develop LLM based models.",
@@ -15,9 +15,9 @@ setup(
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "langchain==0.3.3",
-        "langchain-core==0.3.10",
-        "langchain-openai==0.2.2"
+        "langchain<1.0.0",
+        "langchain-core<1.0.0",
+        "langchain-openai<1.0.0"
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest==8.0.0", "pytest-asyncio==0.23.6"],
